8b726772f3dd999b6eb58cb80abb98ff8f75f8a0
Jens Andresen <jandr@DFDS.COM>
2022-09-22 10:17:01 +0200

    change logging format for production

--- src/Dafda.Gendis.App/Configuration/Serilog.cs
+++ src/Dafda.Gendis.App/Configuration/Serilog.cs
@@ -21,16 +21,14 @@ public static class Serilog
                 .MinimumLevel.Override("Microsoft.Extensions.Diagnostics.HealthChecks", LogEventLevel.Warning)
                 ;
 
-            configuration.WriteTo.Console(theme: AnsiConsoleTheme.Code);
-
-            //if (context.HostingEnvironment.IsProduction())
-            //{
-            //    configuration.WriteTo.Console(new CompactJsonFormatter());
-            //}
-            //else
-            //{
-            //    configuration.WriteTo.Console(theme: AnsiConsoleTheme.Code);
-            //}
+            if (context.HostingEnvironment.IsProduction())
+            {
+                configuration.WriteTo.Console(new CompactJsonFormatter());
+            }
+            else
+            {
+                configuration.WriteTo.Console(theme: AnsiConsoleTheme.Code);
+            }
         });
     }
 }
